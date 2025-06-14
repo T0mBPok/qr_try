6d8e228557e566f783ebd33b5429e116e04d3ec5
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2025-06-14 11:26:47 +0700

    new file:   .gitignore 	new file:   docker-compose.yml 	new file:   src/config.py 	new file:   src/database.py 	new file:   src/main.py

---
+++ src/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env"),
+        env_file_encoding="utf-8"
+    )
+    
+settings = Settings() 
+
+def get_db_url():
+    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
+            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
