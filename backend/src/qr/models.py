f3979e94d960893479971ac58480d7248d1afb99
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2025-12-07 23:13:00 +0700

    modified:   backend/src/migration/env.py 	modified:   backend/src/qr/models.py

--- backend/src/qr/models.py
+++ backend/src/qr/models.py
@@ -6,7 +6,6 @@ class QR(Base):
     id: Mapped[int_pk]
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
     description: Mapped[str | None]
-    src: Mapped[str_uniq]
     link: Mapped[str] = mapped_column(nullable=True)
     
     user: Mapped['User'] = relationship('User', back_populates='qrs')
